fe1017e23c43af471168e9d984556a806c120de8
Joseph Camacho <spongkj@gmail.com>
2024-06-09 21:53:45 +0800

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import { Profile } from './Profile/Profile';
+import user from '../json/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../json/data.json';
+
+import friends from '../json/friends.json';
+import { FriendList } from './FriendList/FriendList';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../json/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +18,22 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload Stats" stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
