5f5081ce339e4093c4cfefc550783ef1aae44f54
Joseph Camacho <spongkj@gmail.com>
2024-06-09 23:30:03 +0800

    adjust-layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,14 +12,14 @@ import transactions from '../json/transactions.json';
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
     >
       <Profile
         username={user.username}
